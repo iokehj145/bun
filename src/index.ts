0d43943d241b31fa41cb22130202ca4bff223aae
Yaric <iiiokehj@gmail.com>
2024-06-15 19:10:19 +0300

    First

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import {cors} from '@elysiajs/cors'
+const app = new Elysia()
+.use(cors())
+.get("/", () => {return {hello : "Hello Elysia"}})
+.listen(4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
