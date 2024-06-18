e53bc1b7650a2dabc6562061ed6f0168262c9335
Yaric <iiiokehj@gmail.com>
2024-06-18 18:55:40 +0300

    Users

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,32 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 const app = new Elysia();
 app.use(cors());
+interface User {
+    name: string;
+    password: string;
+    email: string;
+}
+let users: User[] = [{name: "Yaric", password: "12345", email: "Some@gmail.com"}];
 
 app.get("/", async () => {
-  console.log("Handling GET /");
-  try {
-    return ("hello world");
-  } catch (error) {
-    console.error("Error handling request:", error);
-    throw error;
-  }
-});
+    return ("Hello world, from Yaric!")
+})
+
+app.post("/user", async(request) => {
+ try{
+    const user : User = await (request.body as Promise<User>);
+    users.push(user);
+    console.log(`Add user ${user.name}, email ${user.email}, password ${user.password}`);
+    return (`I make user ${user.name}, email ${user.email}, password ${user.password}`)
+    } 
+    catch(error){
+    return (error);
+ }
+})
+
+app.get("/users", () => {
+    return (users)
+})
 app.listen(8000)
 
 console.log("Server begin");
