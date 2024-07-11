335c7679f5db05d8f06651ab6aa3daf4e041b97b
Yaric <iiiokehj@gmail.com>
2024-07-11 12:54:43 +0300

    version 0.0.1

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Node.js runtime as a parent image
+FROM oven/bun:latest
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and bun.lockb files to the working directory
+COPY package.json bun.lockb ./
+
+# Install the dependencies
+RUN bun install
+
+# Copy the rest of your application code to the working directory
+COPY . .
+
+# Expose the port the app runs on
+EXPOSE 4000
+
+# Define the command to run the application
+CMD ["bun", "run", "src/index.ts"]
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { Database } from "bun:sqlite";
+
+const Users = new Database("./Users.sqlite", {create: true});
+Users.run(`CREATE TABLE IF NOT EXISTS users (name TEXT, password TEXT, email TEXT)`);
+
+const app = new Elysia();
+app.use(cors());
+interface User {
+    name: string;
+    password: string;
+    email: string;
+}
+
+app.get("/", async () => {
+    return ("Hello world, from Yaric!")
+})
+
+app.post("/user", async(request: any) => {
+ try{
+    const user : User = await (request.body as Promise<User>)
+    Users.run(`INSERT INTO USERS VALUES('${user.name}', '${user.password}', '${user.email}')`);
+    console.log(`Add user ${user.name}, email ${user.email}, password ${user.password}`)
+    } 
+    catch(error){
+    console.log(error);
+    return error
+ }
+})
+
+app.get("/users", async() => {
+  try {
+    return await Promise.resolve(Users.query("SELECT * FROM USERS").all())
+    }
+    catch (error){
+    console.log(error);
+    return error
+  }
+})
+
+app.listen(8000, () => {
+    console.log("Server begin")
+});
